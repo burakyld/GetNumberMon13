3783a9907b61eabdf5234e7f4247b719a4dabec7
furkanaydinn <furkan.aydin@bil.omu.edu.tr>
2019-05-13 18:40:38 +0300

    ilk
    SA

---
+++ Mon13.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mon13 {
+
+	public static void main(String[] args) {
+		System.out.println("Kullanicidan Sayi Alan Proje");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bir sayi girin");
+		int sayi = scan.nextInt();
+		System.out.println(sayi);
+
+	}
+
+}
